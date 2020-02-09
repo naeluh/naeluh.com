f53ae99a3a093640e6a9336e2d1e3a6df61c9715
naeluh <naeluh@gmail.com>
2020-02-09 13:13:13 -0800

    add git utils

--- plugins/netlify-plugin-hello-world/index.js
+++ plugins/netlify-plugin-hello-world/index.js
@@ -1,7 +1,37 @@
 module.exports = {
   name: "netlify-plugin-hello-world",
-  onInit: () => {
-    console.log("Do thing on onInit event");
+  onInit: ({ utils }) => {
+    const { git } = utils;
+
+    /* Do stuff if files modified */
+    if (git.modifiedFiles.length) {
+      console.log("Modified files:", git.modifiedFiles);
+    }
+
+    /* Do stuff only if html code edited */
+    const htmlFiles = git.fileMatch("**/*.html");
+    console.log("html files git info:", htmlFiles);
+
+    if (htmlFiles.edited.length !== 0) {
+      console.log(">> Run thing because HTML has changed\n");
+    }
+    //
+    /* Do stuff only if markdown files edited */
+    const markdownFiles = git.fileMatch("**/*.md");
+    console.log("markdown files git info:", markdownFiles);
+
+    if (markdownFiles.modified.length !== 0) {
+      console.log(
+        ">> Run thing because Markdown files have been created/changed/deleted\n"
+      );
+    }
+
+    /* Do stuff only if css files edited */
+    const cssFiles = git.fileMatch("**/*.css");
+    if (cssFiles.deleted.length !== 0) {
+      console.log(">> Run thing because css files have been deleted\n");
+      console.log(cssFiles);
+    }
   },
   onPreBuild: () => {
     console.log("Hello world from onPreBuild event!");
