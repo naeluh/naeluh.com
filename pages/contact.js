73cb05228c9ca21368a7c1455c90aaec7a777d6b
naeluh <naeluh@gmail.com>
2020-03-29 16:09:40 -0700

    add component

--- pages/contact.js
+++ pages/contact.js
@@ -1,16 +1,16 @@
-import App from '../components/App'
-import Header from '../components/Header'
-import Form from '../components/Form'
-import Footer from '../components/Footer'
-import Head from 'next/head'
+import App from '../components/App';
+import Header from '../components/Header';
+import Form from '../components/NetlifyForm';
+import Footer from '../components/Footer';
+import Head from 'next/head';
 
 export default () => (
   <App>
     <Head>
-    <title>Contact</title>
+      <title>Contact</title>
     </Head>
     <Header />
     <Form />
     <Footer />
   </App>
-)
+);
