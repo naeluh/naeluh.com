0d5d4dc43184961088213b494b2a51092f149dff
naeluh <naeluh@gmail.com>
2022-03-06 16:59:05 -0800

    Remove unused font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,6 @@
 import Document, {
   DocumentContext,
   Html,
-  Head,
   Main,
   NextScript,
 } from 'next/document';
@@ -15,17 +14,6 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head>
-          <style
-            dangerouslySetInnerHTML={{
-              __html: `
-              <link rel="preconnect" href="https://fonts.googleapis.com">
-              <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-              <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+Display:wght@400;500;600;800&display=swap" rel="stylesheet">
-              `,
-            }}
-          />
-        </Head>
         <body className="dark">
           <Main />
           <NextScript />
