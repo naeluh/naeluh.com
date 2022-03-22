ae5e7537f0bbe9722d7ec55bc89ccaedf4e4c43e
naeluh <naeluh@gmail.com>
2022-03-21 21:55:05 -0700

    New print styles

--- components/Page/Page.tsx
+++ components/Page/Page.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import rehypeRaw from 'rehype-raw';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 
 const Page: React.FC<{ data: { Title: string; Body: string; id: string } }> = ({
   data,
 }) => {
+  const router = useRouter();
   return (
     <section>
       <Head>
@@ -19,6 +21,20 @@ const Page: React.FC<{ data: { Title: string; Body: string; id: string } }> = ({
           <ReactMarkdown children={data.Body} rehypePlugins={[rehypeRaw]} />
         </div>
       </div>
+      {router.pathname === '/about' && (
+        <div className="id--container">
+          <div className="name">Nick Hulea</div>
+          <div className="phone">
+            <a href="tel:330-207-3406">330-207-3406</a>
+          </div>
+          <div className="email">
+            <a href="mailto:naeluh@gmail.com">naeluh@gmail.com</a>
+          </div>
+          <div className="address">
+            <address>724 Echo Park Ave. Apt. B Los Angeles, CA 90026</address>
+          </div>
+        </div>
+      )}
     </section>
   );
 };
