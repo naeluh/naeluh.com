954005f9d55723eb09200111828d222aa7a14d66
naeluh <naeluh@gmail.com>
2022-04-23 14:00:16 -0700

    Add skip link and a few CSS tweaks

--- components/App/App.tsx
+++ components/App/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import styles from './app.module.css';
 
 const App: React.FC<{ children?: React.ReactNode }> = ({ children }) => (
-  <main>{children}</main>
+  <>
+    <a className={styles.skipNavigationLink} href="#main-content">
+      Skip to Main Content
+    </a>
+    <main id="main-content">{children}</main>
+  </>
 );
 
 export default App;
